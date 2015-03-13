91c055b3956163e733756f7b4b6db9d4524d221e
Jussi Auvinen <jaa49@phy.duke.edu>
2015-03-13 16:07:33 -0400

    ParticleData: Remove exception output
    In cases of bad log values, set value silently
    to 1000 or -1000 (depending on error).
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/dataobjects/particledata.py
+++ hybrid_analysis/dataobjects/particledata.py
@@ -11,30 +11,18 @@ class ParticleData:
             self.rap = 0.5 * math.log((momentum[0] + momentum[3])
                                       / (momentum[0] - momentum[3]))
         except ZeroDivisionError:
-            print "!! zero division error with E", momentum[0],
-            print "and pz", momentum[3]
             self.rap =  1000.0
-            continue
         except ValueError:
-            print "!! log value error with E", momentum[0],
-            print "and pz", momentum[3]
             self.rap =  -1000.0
-            continue
 
         pabs = math.sqrt(momentum[1]**2 + momentum[2]**2 + momentum[3]**2)
         try:
             self.pseudorap = 0.5 * math.log((pabs + momentum[3])
                                             /(pabs - momentum[3]))
         except ZeroDivisionError:
-            print "!! zero division error with pabs", pabs,
-            print "and pz", momentum[3]
             self.pseudorap = 1000.0
-            continue
         except ValueError:
-            print "!! log value error with pabs", pabs, "and pz",
-            print momentum[3]
             self.pseudorap = -1000.0
-            continue
 
         self.ptype = ptype
         self.charge = charge
