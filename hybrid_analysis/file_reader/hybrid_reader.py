fdb63cca26170d1348526ce8c357803ac6b37cf6
Jussi Auvinen <jaa49@phy.duke.edu>
2015-03-13 15:37:36 -0400

    Use ParticleData class in file reader
    File reader now creates lists of ParticleData objects.
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/file_reader/hybrid_reader.py
+++ hybrid_analysis/file_reader/hybrid_reader.py
@@ -3,18 +3,24 @@
 # Functions for reading hybrid model output(s)
 
 import os
+import copy
+from .. import dataobjects.particledata
 
 def read_afterburner_output(filename, read_initial=False):
     id_event = 0
-    particlelist = []
     if os.path.isfile(filename):
         read_data = False
         initial_list = False
+        eventlist = []
+        particlelist = []
         for line in open(filename, "r"):
             inputline = line.split()
             if (inputline and inputline[0] == "#event"):
                 id_event += 1
                 read_data = False
+                if (len(particlelist) > 0):
+                    eventlist.append(copy.copy(particlelist))
+                    del particlelist[:]
             if (inputline and inputline[0] == "#cols:"):
                 if not initial_list:
                     initial_list = True
@@ -29,10 +35,19 @@ def read_afterburner_output(filename, read_initial=False):
 
             if read_data:
                 try:
-                    x = float(inputline[0])
+                    px = float(inputline[4])
+                    py = float(inputline[5])
+                    pz = float(inputline[6])
+                    E = float(inputline[7])
                     ptype = int(inputline[8])
+                    charge = int(inputline[10])
                 except ValueError:
                     continue
-                particlelist.append((tuple(inputline), id_event))
+                particledata = ParticleData([E, px, py, pz], ptype, charge)
+                particlelist.append(particledata)
 
-    return particlelist
+        if (len(particlelist) > 0):
+            eventlist.append(copy.copy(particlelist))
+            del particlelist[:]
+
+    return eventlist
