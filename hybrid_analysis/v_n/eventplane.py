683e37115aa3af13fb0a5e6b290ca8a5bd3a4e4c
Jussi Auvinen <jaa49@phy.duke.edu>
2015-03-23 13:08:27 -0400

    Event plane v_n input parameter update
    Function v2v3_event takes now the following input:
    - list of particles from an event
    - list of v_n-analysis related sums
      (needs to be mutable to be properly updated
       in the main program)
    - pT and rapidity cuts
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/v_n/eventplane.py
+++ hybrid_analysis/v_n/eventplane.py
@@ -3,116 +3,166 @@
 # Functions for computing event plane v2 and v3
 
 import math
+import numpy
 import scipy.special as bessel
 
 # Compute single-event vn quantities
 # Input: List of particles, containing their momentum and azimuthal angle
 # Return: Tuple of <v2>, <v2^2>, <v3>, <v3^2>, and sub-event versions
-def v2v3(particlelist):
-    ncharges = 0
-    v2eventsum = 0
-    v3eventsum = 0
-    v2eventmean = 0
-    v2eventmeansqr = 0
-    v3eventmean = 0
-    v3eventmeansqr = 0
+def v2v3event(particlelist, vn_event_sums,
+              ptmin=0.2, ptmax=2.0, etacut=1.0):
 
+    ncharges = 0
     if (len(particlelist) > 1):
+        filtered_particles = [ x for x in particlelist
+                               if (x.pt > ptmin and x.pt < ptmax
+                                   and abs(x.pseudorap) < etacut) ]
+
+        ptarray = numpy.array([ x.pt for x in filtered_particles ])
+        phiarray = numpy.array([ x.phi for x in filtered_particles ])
+
+        sin2array = numpy.sin(2 * phiarray)
+        cos2array = numpy.cos(2 * phiarray)
+        sin3array = numpy.sin(3 * phiarray)
+        cos3array = numpy.cos(3 * phiarray)
+        ptphisin2 = (numpy.multiply(ptarray, sin2array))
+        ptphicos2 = (numpy.multiply(ptarray, cos2array))
+        ptphisin3 = (numpy.multiply(ptarray, sin3array))
+        ptphicos3 = (numpy.multiply(ptarray, cos3array))
+
         sin2subsum = [0.0] * 2
         cos2subsum = [0.0] * 2
         sin3subsum = [0.0] * 2
         cos3subsum = [0.0] * 2
+        nsub = [0] * 2
 
-        for i in range (0, len(particlelist)):
+        chgphis = []
+        sin2mean = []
+        cos2mean = []
+        sin3mean = []
+        cos3mean = []
+
+        npart = (len(filtered_particles) - 1)
+        for i in range (0, len(filtered_particles)):
             if (i%2 == 0):
                 subgroup = 1
             else:
                 subgroup = 0
-            if (particlelist[i].charge != 0
-                and abs(particlelist[i].pseudorap) < 1):
-
+            if (filtered_particles[i].charge != 0):
                 ncharges += 1
-                sin2sum = 0
-                cos2sum = 0
-                sin3sum = 0
-                cos3sum = 0
+                chgphis.append(filtered_particles[i].phi)
 
-                nparticles[subgroup] += 1
-                sin2subsum[subgroup] += pt_j * math.sin(2 * phi_j)
-                cos2subsum[subgroup] += pt_j * math.cos(2 * phi_j)
-                sin3subsum[subgroup] += pt_j * math.sin(3 * phi_j)
-                cos3subsum[subgroup] += pt_j * math.cos(3 * phi_j)
+                nsub[subgroup] += 1
+                sin2subsum[subgroup] += ptphisin2[i]
+                cos2subsum[subgroup] += ptphicos2[i]
+                sin3subsum[subgroup] += ptphisin3[i]
+                cos3subsum[subgroup] += ptphicos3[i]
 
-                # vn for particle i
-                for j in range (0, len(particlelist)):
-                    # exclude the analysed particle from event plane definition
-                    # to avoid autocorrelations
-                    if (j != i):
+                # exclude the analysed particle from event plane definition
+                # to avoid autocorrelations
 
-                        pt_j = particlelist[j].pt
-                        phi_j = particlelist[j].phi
-                        sin2sum += pt_j * math.sin(2 * phi_j)
-                        cos2sum += pt_j * math.cos(2 * phi_j)
-                        sin3sum += pt_j * math.sin(3 * phi_j)
-                        cos3sum += pt_j * math.cos(3 * phi_j)
+                sin2mean.append((ptphisin2.sum() - ptphisin2[i]) / npart)
+                cos2mean.append((ptphicos2.sum() - ptphicos2[i]) / npart)
+                sin3mean.append((ptphisin3.sum() - ptphisin3[i]) / npart)
+                cos3mean.append((ptphicos3.sum() - ptphicos3[i]) / npart)
 
-
-                # full event value
-                normalization = (len(particlelist) - 1)
-                sin2mean = sin2sum / normalization
-                cos2mean = cos2sum / normalization
-                psitwo = math.atan2(sin2mean, cos2mean) / 2
-                sin3mean = sin3sum / normalization
-                cos3mean = cos3sum / normalization
-                psithree = math.atan2(sin3mean, cos3mean) / 3
-                v2eventsum += math.cos(2 * (particlelist[i].phi - psitwo))
-                v3eventsum += math.cos(3 * (particlelist[i].phi - psithree))
-
-                # subevent value
-                norm_sub = nparticles[subgroup]
-                sin2mean[subgroup] = sin2sum[subgroup] / norm_sub
-                cos2mean[subgroup] = cos2sum[subgroup] / norm_sub
-                psi2[subgroup] = math.atan2(sin2mean[subgroup],
-                                            cos2mean[subgroup]) / 2
-                sin3mean[subgroup] = sin3sum[subgroup] / norm_sub
-                cos3mean[subgroup] = cos3sum[subgroup] / norm_sub
-                psi3[subgroup] = math.atan2(sin3mean[subgroup],
-                                            cos3mean[subgroup]) / 3
-
-                psi2term = math.cos(2 * (psi2[0] - psi2[1]))
-                psi3term = math.cos(3 * (psi3[0] - psi3[1]))
-                v2subsum += psi2term
-                v3subsum += psi3term
-                v2subsumsqr += psi2term**2
-                v3subsumsqr += psi3term**2
+        # full event value
+        chgphiarray = numpy.array(chgphis)
+        sin2meanarray = numpy.array(sin2mean)
+        cos2meanarray = numpy.array(cos2mean)
+        psitwoarray = numpy.arctan2(sin2meanarray, cos2meanarray) / 2
+        sin3meanarray = numpy.array(sin3mean)
+        cos3meanarray = numpy.array(cos3mean)
+        psithreearray = numpy.arctan2(sin3meanarray, cos3meanarray) / 3
+        v2eventsum = numpy.sum(numpy.cos(2 * (chgphiarray - psitwoarray)))
+        v3eventsum = numpy.sum(numpy.cos(3 * (chgphiarray - psithreearray)))
 
         if (ncharges > 0):
-            v2eventmean = v2eventsum / ncharges
-            v2eventmeansqr = (v2eventsum / ncharges)**2
-            v3eventmean = v3eventsum / ncharges
-            v3eventmeansqr = (v3eventsum / ncharges)**2
+            vn_event_sums[0] += v2eventsum / ncharges
+            vn_event_sums[1] += (v2eventsum / ncharges)**2
+            vn_event_sums[2] += v3eventsum / ncharges
+            vn_event_sums[3] += (v3eventsum / ncharges)**2
 
-    return (v2eventmean, v2eventmeansqr, v3eventmean, v3eventmeansqr,
-            v2subsum, v2subsumsqr, v3)
+        psi2sub = [0.0] * 2
+        psi3sub = [0.0] * 2
+        for i in range(0, 2):
+            if (nsub[i] > 0):
+                sin2submean = sin2subsum[i] / nsub[i]
+                cos2submean = cos2subsum[i] / nsub[i]
+                psi2sub[i] = math.atan2(sin2submean, cos2submean) / 2
+                sin3submean = sin3subsum[i] / nsub[i]
+                cos3submean = cos3subsum[i] / nsub[i]
+                psi3sub[i] = math.atan2(sin3submean, cos3submean) / 3
+
+        psi2term = math.cos(2 * (psi2sub[0] - psi2sub[1]))
+        psi3term = math.cos(3 * (psi3sub[0] - psi3sub[1]))
+        vn_event_sums[4] += psi2term
+        vn_event_sums[5] += psi2term**2
+        vn_event_sums[6] += psi3term
+        vn_event_sums[7] += psi3term**2
 
 
-def v2v3mean(vneventslist):
-    meanv2 = 0.0
-    meanv2sqr = 0.0
-    meanv3 = 0.0
-    meanv3sqr = 0.0
-    for vntuple in vneventslist:
-        meanv2 += vntuple[0]
-        meanv2sqr += vntuple[1]
-        meanv3 += vntuple[2]
-        meanv3sqr += vntuple[3]
 
-    meanv2 /= len(vneventslist)
-    meanv2sqr /= len(vneventslist)
-    meanv3 /= len(vneventslist)
-    meanv3sqr /= len(vneventslist)
+def v2v3mean(vn_event_sums, nevents):
 
-    errv2 = math.sqrt((meanv2sqr - meanv2**2) / (len(vneventslist) - 1))
-    errv3 = math.sqrt((meanv3sqr - meanv3**2) / (len(vneventslist) - 1))
+    if nevents > 0:
+        print "Events:", nevents
+        meanv2 = vn_event_sums[0] / nevents
+        meanv2sqr = vn_event_sums[1] / nevents
+        meanv3 = vn_event_sums[2] / nevents
+        meanv3sqr = vn_event_sums[3] / nevents
 
-    return (meanv2, errv2, meanv3, errv3)
+        errv2 = math.sqrt((meanv2sqr - meanv2**2) / (nevents - 1))
+        errv3 = math.sqrt((meanv3sqr - meanv3**2) / (nevents - 1))
+
+        print "Uncorrected v2:", meanv2, "err:", errv2,
+        print "v3:", meanv3, "err:", errv3
+
+        meanv2sub = math.sqrt(vn_event_sums[4] / nevents)
+        meanv3sub = math.sqrt(vn_event_sums[6] / nevents)
+        print "Rsub2:", meanv2sub, "Rsub3:", meanv3sub
+        meanv2subsqr = math.sqrt(vn_event_sums[5] / nevents)
+        meanv3subsqr = math.sqrt(vn_event_sums[7] / nevents)
+
+        v2corr = 0.0
+        v2err = 0.0
+        v3corr = 0.0
+        v3err = 0.0
+
+        print "R2_full chi2 R3_full chi3"
+        v2 = True
+        for vnsub in [meanv2sub, meanv3sub]:
+            chisub = 2.0
+            delta = 8.0
+            for iteration in range(0, 20):
+                arg = chisub**2 / 4
+                result = (math.sqrt(math.pi / 2) / 2 * chisub
+                          * math.exp(-arg) * (bessel.i0(arg) + bessel.i1(arg)))
+                if (result < vnsub):
+                    chisub += delta
+                elif (result > vnsub):
+                    chisub -= delta
+                else:
+                    break
+                delta /= 2
+
+            chifull = math.sqrt(2) * chisub
+            arg = chifull**2 / 4
+            reso_corr = (math.sqrt(math.pi / 2) / 2 * chifull
+                         * math.exp(-arg) * (bessel.i0(arg) + bessel.i1(arg)))
+            print reso_corr, chifull,
+
+            if v2:
+                v2corr = meanv2 / reso_corr
+                v2err = errv2 / reso_corr
+                v2 = False
+            else:
+                v3corr = meanv3 / reso_corr
+                v3err = errv3 / reso_corr
+
+        print ""
+        print "Corrected v2 err v3 err"
+        print v2corr, v2err, v3corr, v3err
+
+    else:
+        print "No events found for event plane v_n determination."
