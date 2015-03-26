a852dae03b6686e5e549edcfacffb5abc9082bcb
Jussi Auvinen <jaa49@phy.duke.edu>
2015-03-26 11:13:08 -0400

    cumulants.py: Add missing pseudorapidity cut and docu
    Implemented the missing pseudorapidity cut to cumulant
    analysis.
    
    While at it, added documentation for the helper function.
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/v_n/cumulants.py
+++ hybrid_analysis/v_n/cumulants.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
-def charged_phis(particlelist, ptmin=0.2, ptmax=2.0):
+# charged_phis: Count the number of charged particles in an event
+# after appropriate transverse momentum and pseudorapidity cuts
+# and return list of azimuthal angles of these particles
+# Needed for cumulant analysis
+#
+# Input:
+# particlelist: List of particle data for one event
+# ptmin: Lower limit of transverse momentum
+# ptmax: Upper limit of transverse momentum
+# etacut: Pseudorapidity cut (assumed symmetric around 0)
+#
+# Output:
+# ncharges: count of charged particles in the event
+# phis_in_event: List of charged particle azimuthal angles
+#                (angle in the plane perpendicular to beam)
+
+def charged_phis(particlelist, ptmin=0.2, ptmax=2.0, etacut=1.0):
     ncharges = 0
     phis_in_event = []
     for particle in particlelist:
-        if ((particle.charge != 0) and particle.pt > ptmin
-            and particle.pt < ptmax):
+        if ((particle.charge != 0)
+            and particle.pt > ptmin and particle.pt < ptmax
+            and abs(particle.pseudorap) < etacut):
             ncharges += 1
             phis_in_event.append(particle.phi)
 
