e4545e4cacbfa9ea792fe64ff46f5957f0db5974
Jussi Auvinen <jaa49@phy.duke.edu>
2015-05-20 15:40:26 -0400

    File reader: Read part of file
    For large files, it would be preferable to be able
    to read the file in parts (event by event).
    
    This in mind, separate the event reading into its own
    function, which takes the file handle as input.
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/file_reader/hybrid_reader.py
+++ hybrid_analysis/file_reader/hybrid_reader.py
@@ -6,48 +6,54 @@ import os
 import copy
 from ..dataobjects import particledata as pd
 
+def next_text_event(filehandle, read_initial=False):
+    read_data = False
+    initial_list = False
+    particlelist = []
+    for line in filehandle:
+        inputline = line.split()
+        if (inputline and inputline[0] == "#event"):
+            read_data = False
+            if (len(particlelist) > 0):
+                return particlelist
+
+        if (inputline and inputline[0] == "#cols:"):
+            if not initial_list:
+                initial_list = True
+                if read_initial:
+                    read_data = True
+            else:
+                initial_list = False
+                if read_initial:
+                    read_data = False
+                else:
+                    read_data = True
+
+        if read_data and len(inputline) == 13:
+            try:
+                px = float(inputline[4])
+                py = float(inputline[5])
+                pz = float(inputline[6])
+                E = float(inputline[7])
+                ptype = int(inputline[8])
+                charge = int(inputline[10])
+            except ValueError:
+                continue
+            particledata = pd.ParticleData([E, px, py, pz], ptype, charge)
+            particlelist.append(particledata)
+
+    if particlelist:
+        return particlelist
+
 def read_afterburner_output(filename, read_initial=False):
-    id_event = 0
     eventlist = []
     if os.path.isfile(filename):
-        read_data = False
-        initial_list = False
-        particlelist = []
-        for line in open(filename, "r"):
-            inputline = line.split()
-            if (inputline and inputline[0] == "#event"):
-                id_event += 1
-                read_data = False
-                if (len(particlelist) > 0):
-                    eventlist.append(copy.copy(particlelist))
-                    del particlelist[:]
-            if (inputline and inputline[0] == "#cols:"):
-                if not initial_list:
-                    initial_list = True
-                    if read_initial:
-                        read_data = True
-                else:
-                    initial_list = False
-                    if read_initial:
-                        read_data = False
-                    else:
-                        read_data = True
+        with open(filename, 'r') as f:
+            particlelist = next_text_event(f, read_initial)
+            while particlelist:
+                eventlist.append(copy.copy(particlelist))
+                particlelist = next_text_event(f, read_initial)
 
-            if read_data and len(inputline) == 13:
-                try:
-                    px = float(inputline[4])
-                    py = float(inputline[5])
-                    pz = float(inputline[6])
-                    E = float(inputline[7])
-                    ptype = int(inputline[8])
-                    charge = int(inputline[10])
-                except ValueError:
-                    continue
-                particledata = pd.ParticleData([E, px, py, pz], ptype, charge)
-                particlelist.append(particledata)
 
-        if (len(particlelist) > 0):
-            eventlist.append(copy.copy(particlelist))
-            del particlelist[:]
 
     return eventlist
