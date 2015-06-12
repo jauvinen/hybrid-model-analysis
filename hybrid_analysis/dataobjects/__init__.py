69d7cb7a4f1685179cc5f533c3d3b92c41712e1f
Jussi Auvinen <jaa49@phy.duke.edu>
2015-06-12 14:45:48 -0400

    Pylint dataobjects cleanup
    Docstrings added for dataobjects module.
    
    Signed-off-by: Jussi Auvinen <jaa49@phy.duke.edu>

--- hybrid_analysis/dataobjects/__init__.py
+++ hybrid_analysis/dataobjects/__init__.py
@@ -1,2 +1,2 @@
+""" Module containing custom data objects used by the analysis routines """
 __all__ = ["particledata"]
-
--- hybrid_analysis/dataobjects/particledata.py
+++ hybrid_analysis/dataobjects/particledata.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
+""" Functions related to ParticleData class """
 
 import math
 
 class ParticleData:
+    """ Class for handling data needed by analysis """
     def __init__(self, momentum, ptype, charge):
         self.pt = math.sqrt(momentum[1]**2 + momentum[2]**2)
         self.phi = math.atan2(momentum[2], momentum[1])
@@ -11,9 +13,9 @@ class ParticleData:
             self.rap = 0.5 * math.log((momentum[0] + momentum[3])
                                       / (momentum[0] - momentum[3]))
         except ZeroDivisionError:
-            self.rap =  1000.0
+            self.rap = 1000.0
         except ValueError:
-            self.rap =  -1000.0
+            self.rap = -1000.0
 
         pabs = math.sqrt(momentum[1]**2 + momentum[2]**2 + momentum[3]**2)
         try:
